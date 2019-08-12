7024eb5b0989cafb5a72a5d0165e3992af40cc2e
Ansel Bridgewater <iamansel@gmail.com>
2019-08-11 22:31:59 -0700

    Added edges to graph

--- project.py
+++ project.py
@@ -6,6 +6,7 @@ from hyphenator import read_patterns_file, parse_word
 def get_edges(syllables):
     """Get the edges by using the hyphenator."""
     start = time.time()
+    edges = {}
     trie = read_patterns_file()
 
     content = []
@@ -16,11 +17,21 @@ def get_edges(syllables):
     for line in content:
         result = parse_word(line, trie)
         word_syllables = result.split("-")
-        if word_syllables[0] == "a":
-            print(word_syllables)
+
+        for syl_ind in range(len(word_syllables) - 1):
+            pre_syl = word_syllables[syl_ind]
+            next_syl = word_syllables[syl_ind + 1]
+            if pre_syl in syllables and next_syl in syllables:
+                edge = (pre_syl, next_syl)
+                if edge in edges:
+                    edges[edge] += 1
+                else:
+                    edges[edge] = 1
 
     end = time.time()
     print(f"{end - start} seconds to process enable1 List")
+
+    return edges
 
 
 def main():
@@ -31,11 +42,16 @@ def main():
                         'be', 'per', 'to']
     syllables_set = set(common_syllables)
 
-    graph = Graph()
+    graph = Graph(weighted=True, directed=True)
     for syllable in common_syllables:
         graph.add_vertex(syllable)
 
-    get_edges(syllables_set)
+    edges = get_edges(syllables_set)
+    for edge in edges:
+        from_vert = edge[0]
+        to_vert = edge[1]
+        weight = edges[edge]
+        graph.add_edge(from_vert, to_vert, weight)
 
 
 if __name__ == '__main__':
