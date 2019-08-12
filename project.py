5c5fd141cfd1da3f1dfe541042e7dd938e182e53
Ansel Bridgewater <iamansel@gmail.com>
2019-08-11 22:18:58 -0700

    Added code from hyphenator and added most common syllables

--- project.py
+++ project.py
@@ -1,18 +1,41 @@
+import time
 from graph import Graph
-import sys
+from hyphenator import read_patterns_file, parse_word
+
+
+def get_edges(syllables):
+    """Get the edges by using the hyphenator."""
+    start = time.time()
+    trie = read_patterns_file()
+
+    content = []
+    with open("enable1.txt") as file:
+        content = [line.rstrip() for line in file]
+        file.close()
+
+    for line in content:
+        result = parse_word(line, trie)
+        word_syllables = result.split("-")
+        if word_syllables[0] == "a":
+            print(word_syllables)
+
+    end = time.time()
+    print(f"{end - start} seconds to process enable1 List")
 
 
 def main():
-    """Run the challenge with a passed in graph file name."""
-    if len(sys.argv) < 2:
-        print("Please enter a file name.")
-    graph_file = sys.argv[1]
+    """Run the project."""
+    common_syllables = ['ing', 'er', 'a', 'ly', 'ed', 'i', 'es', 're', 'tion',
+                        'in', 'e', 'con', 'y', 'ter', 'ex', 'al', 'de', 'com',
+                        'o', 'di', 'en', 'an', 'ty', 'ry', 'u', 'ti', 'ri',
+                        'be', 'per', 'to']
+    syllables_set = set(common_syllables)
 
     graph = Graph()
-    graph.make_graph_from_file(graph_file)
-    is_eulerian = graph.is_eulerian()
+    for syllable in common_syllables:
+        graph.add_vertex(syllable)
 
-    print(f"This graph is Eulerian: {is_eulerian}")
+    get_edges(syllables_set)
 
 
 if __name__ == '__main__':
